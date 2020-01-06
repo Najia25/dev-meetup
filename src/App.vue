5165a1975c7e288cafddd60b23a9961d9cacdbb8
Najia Afrin <najiaafrin2@gmail.com>
2020-01-06 17:49:36 +0600

    navbar done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="sideNav"
+      absolute
+      temporary
+    >
+    <v-list>
+      <v-list-item link v-for="item in menuItems" :key="item.title">
+        <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    </v-navigation-drawer>
+      <v-app-bar app class="primary" dark>
+        <v-app-bar-nav-icon @click ="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon >
+        <v-toolbar-title>Dev Meetup</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only" >
+          <v-btn text v-for="item in menuItems" :key="item.title">
+            <v-icon left dark>{{ item.icon }}</v-icon>
+            {{ item.title }}
+            </v-btn>
+        </v-toolbar-items>
+      </v-app-bar>
+      <main>
+        <router-view />
+      </main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+
+  data: () => ({
+    //
+    sideNav: false,
+    menuItems: [
+      { icon: 'mdi-account-group', title: 'View Meetups' },
+      { icon: 'mdi-map-marker', title: 'Organize Meetup' },
+      { icon: 'mdi-account', title: 'Profile' },
+      { icon: 'mdi-clipboard-account', title: 'Sign up' },
+      { icon: 'mdi-lock-open', title: 'Sign in' }
+    ]
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
